b2b4668b8e07e14717bfb8269a21708e66c33623
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-10-25 13:53:08 +0200

    first cimponents and some state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/Headers.jsx";  
+
 function App() {
 
-    
+    return (
+        <div>
+        <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,6 @@
+export default function Quiz() {
+const [activeQustionIndex, setActiveQuestionIndex] = useState(0);
+const [userAnswers, setUserAnswers] = useState([]);
+
+    return <p>Currently active Question</p>
+}
