9b93e1f9c18140e45609a9bdeef563a79cc817f0
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-10-26 11:53:00 +0100

    adding question timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,5 @@
 import QUESTIONS from "../questions.js";
+import QuestionTimer from "./QuestionTimer.jsx";
 import { useState } from "react";
 import quizCompleteImg from "../assets/quiz-complete.png";
 
@@ -30,6 +31,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={() => handleSelectAnswer(null)}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffleAnswer.map((answer) => (
